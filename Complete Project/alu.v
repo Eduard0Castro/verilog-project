96d10738e338dbd71f085944b890ee0dbf1a269a
Eduardo Castro <90725507+Eduard0Castro@users.noreply.github.com>
2023-07-27 23:48:34 -0300

    Update alu.v

--- Complete Project/alu.v
+++ Complete Project/alu.v
@@ -9,19 +9,19 @@ assign ALU_Out = ALU_Result; // ALU out
 always @(posedge clk)
 begin
   case(ALU_Sel)
-	  4'b0010: // Multiplication
+	  4'b0010: 
 		  ALU_Result = A + B;
-	  4'b0011: // Division
+	  4'b0011: 
 		  ALU_Result = A - B;
-	  4'b0100: // Logical shift left
+	  4'b0100: 
 		  ALU_Result = ~A;
-	  4'b0101: // Logical shift right
+	  4'b0101: 
 		  ALU_Result = ~B;
-	  4'b0110: // Rotate left
+	  4'b0110: 
 		  ALU_Result = A ^ B;
-	  4'b0111: // Rotate right
+	  4'b0111: 
 		  ALU_Result = A|B;
-	  4'b1000: //  Logical and 
+	  4'b1000:
 		  ALU_Result = A & B;				 
 	  default: ALU_Result = 4'b0000; 
   endcase
